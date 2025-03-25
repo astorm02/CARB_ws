a234129b5a1dd99a809a30cffac3bcb2f1412bf5
astorm02 <148171602+astorm02@users.noreply.github.com>
2025-03-25 18:30:16 -0400

    Update tracking_color_object_launch.py

--- src/tracking_control/launch/tracking_color_object_launch.py
+++ src/tracking_control/launch/tracking_color_object_launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
         executable='color_goal_detection',
         name='color_goal_detection_node',
         parameters=[
-            {'color_low': [0, 0, 120]},{'color_high': [70, 70, 255]}, {'object_size_min':30}
+            {'color_low': [150, 700, 255]},{'color_high': [70, 150, 255]}, {'object_size_min':30}
         ],
         output="screen"
     )

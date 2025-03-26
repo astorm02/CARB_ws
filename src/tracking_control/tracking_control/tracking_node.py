7b257c4d2b3c10941570f0b06bc500263f7c300f
astorm02 <148171602+astorm02@users.noreply.github.com>
2025-03-26 15:02:37 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -95,7 +95,8 @@ class TrackingNode(Node):
         # You can decide to filter the detected object pose here
         # For example, you can filter the pose based on the distance from the camera
         # or the height of the object
-        
+        if np.linalg.norm(cp_world) > 2.5 or cp_world[2] > 0.7:
+            return
         
         try:
             # Transform the center point from the camera frame to the world frame
@@ -106,8 +107,7 @@ class TrackingNode(Node):
             self.get_logger().error('Transform Error: {}'.format(e))
             return
 
-        #if np.linalg.norm(cp_world) > 3 or cp_world[2] > 1:
-            #return
+        
         # Get the detected object pose in the world frame
         self.obs_pose = cp_world
 
@@ -121,8 +121,8 @@ class TrackingNode(Node):
         # You can decide to filter the detected object pose here
         # For example, you can filter the pose based on the distance from the camera
         # or the height of the object
-        #if np.linalg.norm(center_points) > 3 or center_points[2] > 1:
-            #return
+        if np.linalg.norm(center_points) > 2.5 or center_points[2] > 0.7:
+            return
         
         try:
             # Transform the center point from the camera frame to the world frame

5ccaf1afde0c731183effc2d697a48dabbea35d4
astorm02 <148171602+astorm02@users.noreply.github.com>
2025-03-27 18:21:48 -0400

    Update tracking_node.py

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -212,7 +212,7 @@ class TrackingNode(Node):
         k_rep = 3
 
         # Obstacle Avoidance Distance
-        obs_avoid_dist = 0.5
+        obs_avoid_dist = 0.8
 
         # Determine Attractive Froce
         F_atr = k_atr*unit_goal_vec
